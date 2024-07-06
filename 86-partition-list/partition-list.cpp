6c22ab7d71490bd6163e9760357bc6fb11cb2bb5
MuhammadBinSikandar <146186703+MuhammadBinSikandar@users.noreply.github.com>
2024-07-06 14:28:12 +0500

    Time: 4 ms (63.83%) | Memory: 13.5 MB (15.62%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -11,21 +11,25 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* smaller = new ListNode(-1);  // Dummy node for the smaller list
-        ListNode* larger = new ListNode(-1);  // Dummy node for the larger list
-        ListNode* smallerHead = smaller;      // Store the head of the smaller list
-        ListNode* largerHead = larger;        // Store the head of the larger list
+        ListNode *lessHead = new ListNode(0);
+        ListNode *greaterHead = new ListNode(0);
+        ListNode *less = lessHead;
+        ListNode *greater = greaterHead;
         while (head != nullptr) {
             if (head->val < x) {
-                smaller->next = new ListNode(head->val);
-                smaller = smaller->next;
+                less->next = head;
+                less = less->next;
             } else {
-                larger->next = new ListNode(head->val);
-                larger = larger->next;
+                greater->next = head;
+                greater = greater->next;
             }
             head = head->next;
         }
-        smaller->next = largerHead->next;  // Combine lists after skipping dummy nodes
-        return smallerHead->next;          // Return the head of the combined list (skipping dummy node)
+        greater->next = nullptr;
+        less->next = greaterHead->next;
+        ListNode* partitionedHead = lessHead->next;
+        delete lessHead;
+        delete greaterHead;
+        return partitionedHead;
     }
 };
