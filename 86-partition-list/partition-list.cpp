542d46ab8f02fa846c756b315a583f7ad1e68850
MuhammadBinSikandar <146186703+MuhammadBinSikandar@users.noreply.github.com>
2024-07-06 14:28:55 +0500

    Time: 0 ms (100.00%) | Memory: 13.4 MB (68.16%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -8,28 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* partition(ListNode* head, int x) {
+//         ListNode *lessHead = new ListNode(0);
+//         ListNode *greaterHead = new ListNode(0);
+//         ListNode *less = lessHead;
+//         ListNode *greater = greaterHead;
+//         while (head != nullptr) {
+//             if (head->val < x) {
+//                 less->next = head;
+//                 less = less->next;
+//             } else {
+//                 greater->next = head;
+//                 greater = greater->next;
+//             }
+//             head = head->next;
+//         }
+//         greater->next = nullptr;
+//         less->next = greaterHead->next;
+//         ListNode* partitionedHead = lessHead->next;
+//         delete lessHead;
+//         delete greaterHead;
+//         return partitionedHead;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode *lessHead = new ListNode(0);
-        ListNode *greaterHead = new ListNode(0);
-        ListNode *less = lessHead;
-        ListNode *greater = greaterHead;
+        ListNode* smaller = new ListNode(-1);  // Dummy node for smaller list
+        ListNode* larger = new ListNode(-1);  // Dummy node for larger list
+        ListNode* smallerHead = smaller;      // Store the head of the smaller list
+        ListNode* largerHead = larger;        // Store the head of the larger list
+
         while (head != nullptr) {
             if (head->val < x) {
-                less->next = head;
-                less = less->next;
+                smaller->next = new ListNode(head->val);
+                smaller = smaller->next;
             } else {
-                greater->next = head;
-                greater = greater->next;
+                larger->next = new ListNode(head->val);
+                larger = larger->next;
             }
             head = head->next;
         }
-        greater->next = nullptr;
-        less->next = greaterHead->next;
-        ListNode* partitionedHead = lessHead->next;
-        delete lessHead;
-        delete greaterHead;
-        return partitionedHead;
+
+        smaller->next = largerHead->next;  // Combine lists after skipping dummy nodes
+        return smallerHead->next;          // Return the head of the combined list (skipping dummy node)
     }
 };
+
