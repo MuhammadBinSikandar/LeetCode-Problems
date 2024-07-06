ecd0eb57eeb388bb5d4ef5e4288b9b8cb82322e5
MuhammadBinSikandar <146186703+MuhammadBinSikandar@users.noreply.github.com>
2024-07-06 14:26:12 +0500

    Time: 3 ms (70.49%) | Memory: 13.5 MB (35.42%) - LeetSync

---
+++ 86-partition-list/partition-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* smaller = new ListNode(-1);  // Dummy node for the smaller list
+        ListNode* larger = new ListNode(-1);  // Dummy node for the larger list
+        ListNode* smallerHead = smaller;      // Store the head of the smaller list
+        ListNode* largerHead = larger;        // Store the head of the larger list
+        while (head != nullptr) {
+            if (head->val < x) {
+                smaller->next = new ListNode(head->val);
+                smaller = smaller->next;
+            } else {
+                larger->next = new ListNode(head->val);
+                larger = larger->next;
+            }
+            head = head->next;
+        }
+        smaller->next = largerHead->next;  // Combine lists after skipping dummy nodes
+        return smallerHead->next;          // Return the head of the combined list (skipping dummy node)
+    }
+};
