f6d2a85a71e00e764d4d544112a6dfafe6bf3d64
MuhammadBinSikandar <146186703+MuhammadBinSikandar@users.noreply.github.com>
2024-07-06 13:54:33 +0500

    Time: 10 ms (20.37%) | Memory: 14 MB (94.20%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -11,21 +11,44 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL) return head; //checks if the head is NULL and returns directly without checking 
-        ListNode* curr = new ListNode(0,head); //creates a dummy node which is linked with the head node
-        ListNode* prev = curr; // a node to store the previous node of the current node
-        while(head!=NULL){ //a loop that operates until the head is not NULL
-            if(head->next!=NULL && head->val == head->next->val){ /* if head is not NULL and the value of head node is equal to the value of the next node */
-                while(head->next!=NULL && head->val==head->next->val){ /* loop that iterates until head's next is not NULL and the value of head node is equal the next node value*/
-                    head=head->next; /* moves the hed node to the next */
+        if (head == NULL)
+            return head; // checks if the head is NULL and returns directly
+                         // without checking
+        ListNode* curr = new ListNode(
+            0, head); // creates a dummy node which is linked with the head node
+        ListNode* prev =
+            curr; // a node to store the previous node of the current node
+        while (head != NULL) { // a loop that operates until the head is not
+                               // NULL
+            if (head->next != NULL &&
+                head->val ==
+                    head->next->val) { /* if head is not NULL and the value of
+                                          head node is equal to the value of the
+                                          next node */
+                while (head->next != NULL &&
+                       head->val ==
+                           head->next
+                               ->val) { /* loop that iterates until head's next
+                                           is not NULL and the value of head
+                                           node is equal the next node value*/
+                    head = head->next;  /* moves the hed node to the next */
                 }
-                prev->next=head->next; /*after the loop finishes we have skipped all the nodes which have the same value and we have the instance of the node prev which has the different value and we skip same values nodes and link the previous node with the next node of all the similar values*/
-            }else{
-                prev=prev->next; /* if the above coniditon is not saitsfies we move the previous node to the next means that curr node and next node have different value*/
+                prev->next =
+                    head->next; /*after the loop finishes we have skipped all
+                                   the nodes which have the same value and we
+                                   have the instance of the node prev which has
+                                   the different value and we skip same values
+                                   nodes and link the previous node with the
+                                   next node of all the similar values*/
+            } else {
+                prev = prev->next; /* if the above coniditon is not saitsfies we
+                                      move the previous node to the next means
+                                      that curr node and next node have
+                                      different value*/
             }
-            head=head->next; // then we move the head to the next node 
-        }    
-        return curr->next; // return the next node of the dummy node we created at the start
-
+            head = head->next; // then we move the head to the next node
+        }
+        return curr->next; // return the next node of the dummy node we created
+                           // at the start
     }
 };
